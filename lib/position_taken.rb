7f080fead91a3da062e375429210b5d2836ade23
Rob Desz <rob.desz@gmail.com>
2020-09-29 15:43:40 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,5 @@
 def position_taken?(board,index)
   if board[index] == " "
-    puts false
-  elsif board[index] == ""
-    puts false
-  elsif board[index] == nil
-    puts false
+    returns false
   end
 end
